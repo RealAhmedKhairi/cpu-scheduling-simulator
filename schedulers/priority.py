972a6c51eb37a7e869132d419872185efc1d19d0
Ahmed <ahmedkhairi1122@gmail.com>
2026-05-05 20:34:15 +0300

    Adding all  needed material to start.

---
+++ schedulers/priority.py
@@ -0,0 +1,5 @@
+def run_priority(processes, preemptive=False):
+    # lower number = higher priority
+    # returns same format as sjf
+    return timeline, results
+
---
+++ schedulers/sjf.py
@@ -0,0 +1,6 @@
+def run_sjf(processes, preemptive = False):
+    # Processes = list of Process objects
+    # returns: 
+    # timeline = list of (pid, start, end) tuples
+    # results = list of Process objects with .start .finish .waiting .turnaround .filled in
+    return timeline, results
